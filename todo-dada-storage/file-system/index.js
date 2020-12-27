61607aa858ccf859abc24c13fc6bc3aa24d9d3a3
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-12-27 22:21:00 +0900

    fix

--- todo-dada-storage/file-system/index.js
+++ todo-dada-storage/file-system/index.js
@@ -1,6 +1,6 @@
 "use strict"
 const { extname } = require("path")
-const { readdir, readFile } = require("fs").promises
+const { readdir, readFile, writeFile } = require("fs").promises
 
 exports.fetchAll = async () => {
   const files = (await readdir(__dirname))
@@ -9,5 +9,25 @@ exports.fetchAll = async () => {
     files.map(file =>
       readFile(`${__dirname}/${file}`, "utf8").then(JSON.parse)
     )
+  )
+}
+
+exports.fetchByCompleted = completed => exports.fetchAll()
+  .then(all => all.filter(todo => todo.completed === completed))
+
+exports.create = todo =>
+  writeFile(`${__dirname}/${todo.id}.json`, JSON.stringify(todo))
+
+exports.update = async (id, update) => {
+  const fileName = `${__dirname}/${id}.json`
+  return readFile(fileName, "utf8").then(
+    content => {
+      const todo = {
+        ...JSON.parse(content),
+        ...update
+      }
+      return writeFile(fileName, JSON.stringify(todo)).then(()=> todo)
+    },
+    err => err.code === "ENOENT" ? null : Promise.reject(err)
   )
 }
